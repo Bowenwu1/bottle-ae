6a0527a4e2f5dea913285e04fc42c6261b9ec845
吴博文 <bowen.wu@163.com>
2017-12-25 22:55:21 +0800

    项目初始化

---
+++ app/src/main/java/com/pear/bottle_ae/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pear.bottle_ae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
