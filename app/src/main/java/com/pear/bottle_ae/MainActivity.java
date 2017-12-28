0c60fa1e671179e5b44f2e6c48dcc255e8f1ccc1
CodingMie <544286175@qq.com>
2017-12-28 14:33:47 +0800

    个人页面

--- app/src/main/java/com/pear/bottle_ae/MainActivity.java
+++ app/src/main/java/com/pear/bottle_ae/MainActivity.java
@@ -1,13 +1,74 @@
 package com.pear.bottle_ae;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationView bottomNavigationView;
+    private View homePage, personalPage,bottlePage;
+    private List<View> viewList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LayoutInflater inflater = getLayoutInflater();
+        homePage = inflater.inflate(R.layout.activity_personal, null);
+        viewList = new ArrayList<>();
+        viewList.add(homePage);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MyPagerAdapter(viewList));
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.personal_page:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.bottle:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.home_page:
+                        viewPager.setCurrentItem(2);
+                        break;
+                }
+                return false;
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
     }
 }
+
