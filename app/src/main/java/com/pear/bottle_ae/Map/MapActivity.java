7ed179bedb6306bc4a049ab96a66317edd9e441b
Bob Wu <bowen.wu@163.com>
2018-01-02 23:09:06 +0800

    添加multidex

---
+++ app/src/main/java/com/pear/bottle_ae/Map/MapActivity.java
@@ -0,0 +1,51 @@
+package com.pear.bottle_ae.Map;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.maps2d.MapView;
+import com.pear.bottle_ae.R;
+
+/**
+ * Created by wubowen on 2018/1/2.
+ */
+
+public class MapActivity extends AppCompatActivity {
+    private MapView mapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mapView = (MapView)findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
+    }
+
+}
